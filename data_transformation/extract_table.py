c39d370d9f99efe8a787e1597ae3ee6744b2f0ef
LucasPetruci <lucaspetruci100@gmail.com>
2025-03-25 17:34:30 -0300

    feat: add footer extraction functionality

--- data_transformation/extract_table.py
+++ data_transformation/extract_table.py
@@ -1,6 +1,7 @@
 import tabula
 import zipfile
 import os
+import pdfplumber
 
 # Extract tables from PDF file and save it as CSV file
 def convert_pdf_to_csv(pdf_path, output_csv):
@@ -18,5 +19,30 @@ def csv_to_zip(csv_name, zip_name):
         zipf.write(csv_name, os.path.basename(csv_name))
     
     print(f"{csv_name} saved as {zip_name}")
+    
+def extract_footer(pdf_path):
+    with pdfplumber.open(pdf_path) as pdf:
+        last_page = pdf.pages[-1]  
+        footer_text = last_page.extract_text() 
+        
+        if "Legenda" in footer_text:
+            # All text after "Legenda:"
+            legend_text = footer_text.split("Legenda:")[1]
+            
+            abbreviations = {}
+            
+            # All text after "OD:" and before "HSO"
+            od_description = legend_text.split("OD:")[1].split("HSO")[0].strip() 
+            
+            # All text after "AMB:" and before "REF"
+            amb_description = legend_text.split("AMB:")[1].split("REF")[0].strip()
+            
+            abbreviations["OD"] = od_description
+            abbreviations["AMB"] = amb_description
+            
+            print(abbreviations)
+            return abbreviations
+       
+    
         
     
