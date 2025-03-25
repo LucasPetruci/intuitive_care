5f5b7d2cc25fe3b8f4e8240660f65411df9c193d
LucasPetruci <lucaspetruci100@gmail.com>
2025-03-25 01:00:22 -0300

    feat: implement main function for web scraping

---
+++ web_scraping/main.py
@@ -0,0 +1,13 @@
+from driver_config import get_driver
+from scraper import open_url, wait_and_close, get_pdf_link, download_pdf
+
+def main():
+    url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
+    driver = get_driver()
+    open_url(driver, url)
+    links =get_pdf_link(driver)
+    download_pdf(links, "downloads")
+    wait_and_close(driver, 5)
+    
+if __name__ == "__main__":
+    main()
