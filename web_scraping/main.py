65270eaba01d4b9c014fde956fd830e10627a3f5
LucasPetruci <lucaspetruci100@gmail.com>
2025-03-25 13:39:56 -0300

    reafactor: close driver before dowload and compress file

--- web_scraping/main.py
+++ web_scraping/main.py
@@ -4,11 +4,17 @@ from scraper import open_url, wait_and_close, get_pdf_link, download_pdf, compre
 def main():
     url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
     driver = get_driver()
+    
+    # Open the URL and extract the PDF links
     open_url(driver, url)
     links_pdf = get_pdf_link(driver)
+    
+    # Close the browser after 5 seconds
+    wait_and_close(driver, 5)
+    
+    # Download the PDF files and compress them into a ZIP file
     download_pdf(links_pdf, "downloads")
     compress_file("downloads", "downloads.zip")
-    wait_and_close(driver, 5)
     
 if __name__ == "__main__":
     main()
