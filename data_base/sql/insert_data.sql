b3f2fe9f2acdbcf7c36926d0d17f90bd95575bfa
LucasPetruci <lucaspetruci100@gmail.com>
2025-03-26 16:45:01 -0300

    feat: insert csv 1T2023 and 2t2023 to demonstracoes_contabeis

--- data_base/sql/insert_data.sql
+++ data_base/sql/insert_data.sql
@@ -27,6 +27,40 @@ ENCODING 'UTF8';
 
 -- SELECT * FROM operadoras_ativas;
 
+1T2023
+COPY demonstracoes_contabeis(
+  data,
+  registro_ans,
+  cd_conta_contabil,
+  descricao,
+  vl_saldo_inicial,
+  vl_saldo_final
+)
+FROM '/var/lib/postgresql/data/1T2023.csv'
+DELIMITER ';'
+CSV HEADER
+ENCODING 'UTF8';
+
+-- 2t2023
+COPY demonstracoes_contabeis(
+  data,
+  registro_ans,
+  cd_conta_contabil,
+  descricao,
+  vl_saldo_inicial,
+  vl_saldo_final
+)
+FROM '/var/lib/postgresql/data/2t2023.csv'
+DELIMITER ';'
+CSV HEADER
+ENCODING 'UTF8';
+
+-- SELECT * FROM demonstracoes_contabeis;
+
+-- SELECT *
+-- FROM demonstracoes_contabeis
+-- ORDER BY data DESC
+-- LIMIT 10;
 
 
 
