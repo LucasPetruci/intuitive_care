bbac194874bdc26ee34bc2792cd5b7724ba93848
LucasPetruci <lucaspetruci100@gmail.com>
2025-03-27 18:00:31 -0300

    feat: add function to read CSV files with error handling

---
+++ api/backend/read_csv.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+def read_csv(path):
+    try:
+        df = pd.read_csv(
+            path, 
+            sep=';', 
+            encoding='utf-8',
+            dtype=str
+        )
+        df = df.replace([np.inf, -np.inf], np.nan)
+        df = df.where(pd.notnull(df), None)
+    
+        return df
+    except Exception as e:
+        return {"error": str(e)}
