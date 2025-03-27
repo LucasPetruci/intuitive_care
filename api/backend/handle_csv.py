a199b2fbb60ec5d9b624edb41586eb37773f2a07
LucasPetruci <lucaspetruci100@gmail.com>
2025-03-27 18:59:24 -0300

    feat: add search functionality for CSV data

--- api/backend/handle_csv.py
+++ api/backend/handle_csv.py
@@ -15,3 +15,29 @@ def read_csv(path):
         return df
     except Exception as e:
         raise Exception(f"Failed to read CSV: {str(e)}")
+    
+def search_in_csv(search_value: str, column_search: str, path: str):
+    try:
+        df = read_csv(path)
+        # Remove spaces and underscores and convert to lowercase
+        column_search = column_search.strip().lower().replace(" ", "").replace("_", "")
+        df_columns_lower = [col.strip().lower().replace(" ", "").replace("_", "") 
+                          for col in df.columns]
+        matching_columns = [real_col for real_col, lower_col in zip(df.columns, df_columns_lower)
+                          if lower_col == column_search]
+        
+        if not matching_columns:
+            available_columns = ", ".join(df.columns)
+            raise Exception(f"Coluna '{column_search}' não encontrada. Colunas disponíveis: {available_columns}")
+        
+        real_column_name = matching_columns[0]
+
+        search_value = search_value.lower()
+        results = df[
+            df[real_column_name].astype(str).str.lower().str.contains(search_value, na=False)
+        ]
+        
+        return results.to_dict(orient='records')
+
+    except Exception as e:
+        raise Exception(f"Failed to search in CSV: {str(e)}")
--- api/backend/server.py
+++ api/backend/server.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI, HTTPException
 import pandas as pd
-from handle_csv import read_csv
+from handle_csv import read_csv, search_in_csv
 
 app = FastAPI()
 path = "../../data_base/data/operadoras_ativas/Relatorio_cadop.csv"
-
-@app.get("/")
-def read_root():
+    
+@app.get("/search")
+def search_endpoint(search: str, column: str):
     try:
-        data = read_csv(path)
-        return data.to_dict(orient='records')
+        return search_in_csv(
+            search_value=search,
+            column_search=column,
+            path=path,
+        )
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e))
    
