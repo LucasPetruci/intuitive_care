782f94f76b5a573a4a33e92c752941b7f9b4e23b
LucasPetruci <lucaspetruci100@gmail.com>
2025-03-27 12:26:46 -0300

    feat: add new SQL query to calculate total expenses for health assistance events in 2024

--- data_base/sql/queries.sql
+++ data_base/sql/queries.sql
@@ -1,7 +1,9 @@
+-- Query 1
 SELECT 
     oa.registro_ans,
     oa.razao_social,
-    SUM(ABS(vl_saldo_final::DECIMAL - vl_saldo_inicial::DECIMAL)) AS total_despesas
+    SUM(ABS(vl_saldo_final::DECIMAL - vl_saldo_inicial::DECIMAL)) AS total_despesas,
+    'Ultimo trimestre de 2024' AS periodo_referencia
 FROM 
     demonstracoes_contabeis dc
 JOIN 
@@ -9,6 +11,25 @@ JOIN
 WHERE 
     dc.descricao LIKE '%EVENTOS%SINISTROS%CONHECIDOS OU AVISADOS%ASSISTÊNCIA A SAÚDE%MEDICO HOSPITALAR%'
     AND dc.data BETWEEN '2024-10-01' AND '2024-12-31'
+GROUP BY 
+    oa.registro_ans, oa.razao_social
+ORDER BY 
+    total_despesas DESC
+LIMIT 10;
+
+-- Query 2
+SELECT 
+    oa.registro_ans,
+    oa.razao_social,
+    SUM(ABS(vl_saldo_final::DECIMAL - vl_saldo_inicial::DECIMAL)) AS total_despesas,
+    'Ano de 2024' AS periodo_referencia
+FROM 
+    demonstracoes_contabeis dc
+JOIN 
+    operadoras_ativas oa ON dc.registro_ans = oa.registro_ans
+WHERE 
+    dc.descricao LIKE '%EVENTOS%SINISTROS%CONHECIDOS OU AVISADOS%ASSISTÊNCIA A SAÚDE%MEDICO HOSPITALAR%'
+    AND dc.data BETWEEN '2024-01-01' AND '2024-12-31'
 GROUP BY 
     oa.registro_ans, oa.razao_social
 ORDER BY 
