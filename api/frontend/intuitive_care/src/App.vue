400eddec6bc8982aac92d7f648f669dbf8248bf4
LucasPetruci <lucaspetruci100@gmail.com>
2025-03-28 17:19:31 -0300

    feat: add HeaderComponent with logo and styling

--- api/frontend/intuitive_care/src/App.vue
+++ api/frontend/intuitive_care/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
+    <HeaderComponent />
     <SearchComponent />
   </div>
 </template>
 
 <script>
+import HeaderComponent from "./components/HeaderComponent.vue";
 import SearchComponent from "./components/SearchComponent.vue";
 
 export default {
   name: "App",
   components: {
+    HeaderComponent,
     SearchComponent,
   },
 };
---
+++ api/frontend/intuitive_care/src/components/HeaderComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="main-header">
+    <img src="../assets/intuitive-care.png" alt="Logo" class="logo" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+
+<style scoped>
+.main-header {
+  display: flex;
+  align-items: flex-start;
+  padding: 1rem;
+  background-color: white; 
+  border-bottom: 1px solid #ccc
+}
+
+.logo {
+  padding-left: 4rem;
+  height: 50px; 
+  margin-right: 1rem;
+}
+
+</style>
