d60f038c9dc59e0758e5ecad6ef67f51dc60bed8
LucasPetruci <lucaspetruci100@gmail.com>
2025-03-28 19:26:48 -0300

    feat: add FooterComponent to enhance app layout

--- api/frontend/intuitive_care/src/App.vue
+++ api/frontend/intuitive_care/src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <HeaderComponent />
     <SearchComponent />
+    <FooterComponent />
   </div>
 </template>
 
 <script>
 import HeaderComponent from "./components/HeaderComponent.vue";
 import SearchComponent from "./components/SearchComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
 
 export default {
   name: "App",
   components: {
     HeaderComponent,
     SearchComponent,
+    FooterComponent
   },
 };
 </script>
---
+++ api/frontend/intuitive_care/src/components/FooterComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="main-footer">
+    <p class="footer-text">Intuitive Care Inteligência Hospitalar S/A</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent",
+};
+</script>
+
+<style scoped>
+
+.main-footer {
+  width: 100%;
+  padding: 0.1rem;
+  background-color: #c8a4f4;
+  text-align: center;
+  z-index: 10;
+  margin-top: 1rem;
+}
+.footer-text {
+  font-size: 1rem;
+  color: #333;
+}
+</style>
