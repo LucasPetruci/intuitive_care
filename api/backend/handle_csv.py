e56db9c958e21a502c50c47178776543d977a5c1
LucasPetruci <lucaspetruci100@gmail.com>
2025-03-28 14:28:27 -0300

    refactor: update to use oriented object

--- api/backend/handle_csv.py
+++ api/backend/handle_csv.py
@@ -1,51 +1,57 @@
 import pandas as pd
 import numpy as np
 
-def read_csv(path):
-    try:
-        df = pd.read_csv(
-            path, 
-            sep=';', 
-            encoding='utf-8',
-            dtype=str
-        )
-        df = df.replace([np.inf, -np.inf], np.nan)
-        df = df.where(pd.notnull(df), None)
+class HandleCSV:
     
-        return df
-    except Exception as e:
-        raise Exception(f"Failed to read CSV: {str(e)}")
-    
-def search_in_csv(search_value: str, column_search: str, path: str):
-    try:
-        df = read_csv(path)
+    def __init__(self, path: str):
+        self.path = path
+        self.df = self._read()
+
+    #this method read the csv file
+    def _read(self) -> pd.DataFrame:
+        try:
+            df = pd.read_csv(
+                self.path, 
+                sep=';', 
+                encoding='utf-8',
+                dtype=str
+            )
+            df = df.replace([np.inf, -np.inf], np.nan)
+            df = df.where(pd.notnull(df), None)
         
-        # Check if column_search is empyt
-        if not column_search.strip():
+            return df
+        except Exception as e:
+            raise Exception(f"Failed to read CSV: {str(e)}")
+    
+    def search(self, search_value: str, column_search: str):
+        try:
+            
+            # Check if column_search is empyt
+            if not column_search.strip():
+                search_value = search_value.lower()
+                mask = self.df.astype(str).apply(lambda col: col.str.lower().str.contains(search_value, na=False))
+                results = self.df[mask.any(axis=1)]
+                return results.to_dict(orient='records')
+                
+            # Remove spaces and underscores and convert to lowercase
+            column_search = column_search.strip().lower().replace(" ", "").replace("_", "")
+            df_columns_lower = [col.strip().lower().replace(" ", "").replace("_", "") 
+                            for col in self.df.columns]
+            matching_columns = [real_col for real_col, lower_col in zip(self.df.columns, df_columns_lower)
+                            if lower_col == column_search]
+            
+            if not matching_columns:
+                available_columns = ", ".join(self.df.columns)
+                raise Exception(f"Coluna '{column_search}' não encontrada. Colunas disponíveis: {available_columns}")
+            
+            real_column_name = matching_columns[0]
+
             search_value = search_value.lower()
-            mask = df.astype(str).apply(lambda col: col.str.lower().str.contains(search_value, na=False))
-            results = df[mask.any(axis=1)]
+            results = self.df[
+                self.df[real_column_name].astype(str).str.lower().str.contains(search_value, na=False)
+            ]
+            
             return results.to_dict(orient='records')
-               
-        # Remove spaces and underscores and convert to lowercase
-        column_search = column_search.strip().lower().replace(" ", "").replace("_", "")
-        df_columns_lower = [col.strip().lower().replace(" ", "").replace("_", "") 
-                          for col in df.columns]
-        matching_columns = [real_col for real_col, lower_col in zip(df.columns, df_columns_lower)
-                          if lower_col == column_search]
-        
-        if not matching_columns:
-            available_columns = ", ".join(df.columns)
-            raise Exception(f"Coluna '{column_search}' não encontrada. Colunas disponíveis: {available_columns}")
-        
-        real_column_name = matching_columns[0]
 
-        search_value = search_value.lower()
-        results = df[
-            df[real_column_name].astype(str).str.lower().str.contains(search_value, na=False)
-        ]
-        
-        return results.to_dict(orient='records')
-
-    except Exception as e:
-        raise Exception(f"Failed to search in CSV: {str(e)}")
+        except Exception as e:
+            raise Exception(f"Failed to search in CSV: {str(e)}")
--- api/backend/server.py
+++ api/backend/server.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI, HTTPException
-from handle_csv import search_in_csv
+from handle_csv import HandleCSV
 
 app = FastAPI()
 path = "../../data_base/data/operadoras_ativas/Relatorio_cadop.csv"
+
+csv_handler = HandleCSV(path)
     
 @app.get("/search")
 def search_endpoint(search: str, column: str):
     try:
-        return search_in_csv(
+        result =  csv_handler.search(
             search_value=search,
             column_search=column,
-            path=path,
         )
+        return result
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
    
