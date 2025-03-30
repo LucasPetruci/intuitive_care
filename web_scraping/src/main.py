6bca70bf8255a10d68cd10852f3304f335f90d15
LucasPetruci <lucaspetruci100@gmail.com>
2025-03-30 15:15:10 -0300

    refactor: encapsulate web scraping logic within Scraper class

--- web_scraping/src/main.py
+++ web_scraping/src/main.py
@@ -1,20 +1,21 @@
 from driver_config import get_driver
-from scraper import open_url, wait_and_close, get_pdf_link, download_pdf, compress_file
+from scraper import Scraper
 import config
 
 def main():
     driver = get_driver()
+    scraper = Scraper(driver)
     
     # Open the URL and extract the PDF links
-    open_url(driver, config.BASE_URL)
-    links_pdf = get_pdf_link(driver)
+    scraper.open_url(config.BASE_URL)
+    links_pdf = scraper.get_pdf_links()
     
     # Close the browser after 5 seconds
-    wait_and_close(driver, config.WAIT_SECONDS)
+    scraper.wait_and_close(config.WAIT_SECONDS)
     
     # Download the PDF files and compress them into a ZIP file
-    download_pdf(links_pdf, config.DOWLOAD_FOLDER)
-    compress_file("downloads", config.ZIP_FILE_NAME)
+    scraper.download_pdfs(links_pdf, config.DOWLOAD_FOLDER)
+    scraper.compress_files(config.DOWLOAD_FOLDER, config.ZIP_FILE_NAME)
     
 if __name__ == "__main__":
     main()
--- web_scraping/src/scraper.py
+++ web_scraping/src/scraper.py
@@ -1,44 +1,48 @@
-import time
 import os
 import requests
 import zipfile
+import time
 from selenium.webdriver.common.by import By
 from urllib.parse import urljoin
 
-def open_url(driver, url):
-    driver.get(url)
-    return driver.title
+class Scraper:
+    def __init__(self, driver):
+        self.driver = driver
 
-def get_pdf_link(driver):
-    pdf_links = []
-    links = driver.find_elements(By.TAG_NAME, "a")
-    for link in links:
-        text = link.text.strip()
-        href = link.get_attribute("href")
-        if href and href.lower().endswith(".pdf") and ("Anexo I" in text or "Anexo II" in text):
-            absolute_link = urljoin(driver.current_url, href)
-            pdf_links.append(absolute_link)
-    return pdf_links
+    def open_url(self, url):
+        self.driver.get(url)
+        return self.driver.title
 
-def download_pdf(pdf_link, download_folder):
-    for link in pdf_link:
+    def get_pdf_links(self):
+        pdf_links = []
+        links = self.driver.find_elements(By.TAG_NAME, "a")
+        for link in links:
+            text = link.text.strip()
+            href = link.get_attribute("href")
+            if href and href.lower().endswith(".pdf") and ("Anexo I" in text or "Anexo II" in text):
+                absolute_link = urljoin(self.driver.current_url, href)
+                pdf_links.append(absolute_link)
+        return pdf_links
+
+
+    def download_pdfs(self, pdf_links, download_folder):
         os.makedirs(download_folder, exist_ok=True)
-        filename = link.split("/")[-1]
-        filepath = os.path.join(download_folder, filename)
-        
-        response = requests.get(link)
-        if response.status_code == 200:
-            with open(filepath, "wb") as f:
-                f.write(response.content)
-        else:
-            return f"Erro ao baixar o arquivo: {response.status_code, response.reason}"
+        for link in pdf_links:
+            filename = link.split("/")[-1]
+            filepath = os.path.join(download_folder, filename)
+            response = requests.get(link)
+            if response.status_code == 200:
+                with open(filepath, "wb") as f:
+                    f.write(response.content)
+            else:
+                print(f"Erro ao baixar o arquivo: {response.status_code}, {response.reason}")
 
-def compress_file(download_folder, zip_filename):
-    with zipfile.ZipFile(zip_filename, "w") as zipf:
-        for root, _, files in os.walk(download_folder):
-            for file in files:
-                zipf.write(os.path.join(root, file), file)
-    
-def wait_and_close(driver, seconds):
-    time.sleep(seconds)
-    driver.close()
+    def compress_files(self, download_folder, zip_filename):
+        with zipfile.ZipFile(zip_filename, "w") as zipf:
+            for root, _, files in os.walk(download_folder):
+                for file in files:
+                    zipf.write(os.path.join(root, file), file)
+
+    def wait_and_close(self, seconds):
+        time.sleep(seconds)
+        self.driver.close()
